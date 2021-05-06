fdedcf9bbf16fe9cb0ffd7773df9ec411719526e
l08084 <l08084.1989@gmail.com>
2021-05-06 18:16:21 +0900

    fix

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+
+declare var CacheDelete: any;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(private platform: Platform) {}
 
-  constructor() {}
+  public ngOnInit(): void {
+    this.platform.ready().then(() => {
+      if (this.platform.is('android')) {
+        CacheDelete.deleteCache(this.successCallback, this.errorCallback);
+      }
+    });
+  }
 
+  private successCallback(): void {
+    console.log('success');
+  }
+
+  private errorCallback(): void {
+    console.log('error');
+  }
 }
